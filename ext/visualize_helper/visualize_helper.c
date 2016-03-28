5b48c557d274d1c322ae928ae573bd0fb87f3e25
Raphael Ottoni <rapha@Westeros.local>
2016-03-28 19:47:47 -0300

    Adding method to iterate in ruby hashes

--- ext/visualize_helper/visualize_helper.c
+++ ext/visualize_helper/visualize_helper.c
@@ -8,21 +8,43 @@ static VALUE hello_world()
 
 
 
+// Function callback to iterate in hash
+static int testando_hash(VALUE key, VALUE value, VALUE array){
+ 
+  rb_ary_push(array,value);  
+  
+
+  return ST_CONTINUE;
+}  
+
 // 
-static VALUE teste(VALUE self, VALUE min, VALUE max)
+static VALUE teste(VALUE self, VALUE min, VALUE max, VALUE hash)
 {
 
     int  min_c = FIX2INT(min);
     int  max_c = FIX2INT(max);
+    int period = Qnil;
+    VALUE array  = rb_ary_new();
+
+    rb_hash_foreach(hash,testando_hash,array);
+      
+    return array;
+}
 
 
-    //VALUE str1 = rb_str_new_cstr("OLá ...");
-    //VALUE str2 = rb_str_new_cstr("enfermeira!");
+// Function to convert params into array 
+static VALUE create_array(VALUE self, VALUE min, VALUE max)
+{
+
+    int  min_c = FIX2INT(min);
+    int  max_c = FIX2INT(max);
+    int period = Qnil;
+    VALUE array  = rb_ary_new();
+
 
     VALUE str1 = INT2FIX(min_c);
     VALUE str2 = INT2FIX(max_c);
-    
-    VALUE array  = rb_ary_new();
+
     rb_ary_push(array,str1);  
     rb_ary_push(array,str2); 
 
@@ -35,12 +57,12 @@ void Init_visualize_helper(void) {
   // Register the VisualizeHelper module
   VALUE mVisualizeHelper = rb_define_module("VisualizeHelper");
 
-  // Register the module Hello World without parameters 
+  // Register the method Hello World without parameters 
   rb_define_singleton_method(mVisualizeHelper, "hello_world", hello_world, 0);
 
-  // Register the module Hello World with parameters 
-  rb_define_singleton_method(mVisualizeHelper, "teste", teste, 2);
+  // Register the method  teste
+  rb_define_singleton_method(mVisualizeHelper, "teste", teste, 3);
 
-  // Register the module Hello World without parameters 
-  //rb_define_singleton_method(mVisualizeHelper, "descobre", descobre, 2);
+  // Register the method create_array 
+  rb_define_singleton_method(mVisualizeHelper, "create_array", create_array, 2);
 }
