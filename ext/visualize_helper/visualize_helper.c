bb3600f59930082f8b06d9e25d26cf046f26a536
Raphael Ottoni <rapha@Westeros.local>
2016-03-29 11:36:43 -0300

    working on min_max_period

--- ext/visualize_helper/visualize_helper.c
+++ ext/visualize_helper/visualize_helper.c
@@ -6,50 +6,104 @@ static VALUE hello_world()
     return rb_str_new_cstr("hello world");
 }
 
+// Find the index of the target value inside array
+int findIndex(VALUE intervals, size_t size, int target) 
+{
+      int i=0;
+       while((i<size) && (FIX2INT(rb_ary_entry(intervals,i)) != target)) i++;
+
+       return (i<size) ? (i) : (-1);
+}
 
 
 // Function callback to iterate in hash
-static int testando_hash(VALUE key, VALUE value, VALUE array){
+// Params: 
+//        days: hash key (VALUE)
+//        traj: hash value (VALUE)
+//        array: [min (int) ,max (int) ,period (Qnil/int), intervals (C array), intervals_size(int), aggr (VALUE)]
+static int encontra_min_max_period(VALUE days, VALUE traj, VALUE array){
  
-  rb_ary_push(array,value);  
+  // Convert every variable need to C value
+  VALUE intervals = rb_ary_entry(array,3);
+  VALUE aggr = rb_ary_entry(array,4);
+  int intervals_size = RARRAY_LEN(array);
+  int aggr_size = RARRAY_LEN(aggr);
+  VALUE first_interval = rb_ary_entry(intervals,0);
+  VALUE last_interval = rb_ary_entry(intervals,intervals_size - 1);
+  int first_interval_c = FIX2INT(first_interval);
+  int last_interval_c = FIX2INT(last_interval);
+  int days_c = atoi(StringValuePtr(days));
+  int min_c = FIX2INT(rb_ary_entry(array,0));
+  int max_c = FIX2INT(rb_ary_entry(array,1));
+  int teste[3] = {1,2,0};
+
+
+  if (days_c < first_interval_c) {
+    if (min_c > days_c) {
+      rb_ary_store(array,0,days);
+    }
+    rb_ary_store(array,2,INT2FIX(0));
+  }else if (days_c == 0) {
+    int index = findIndex(intervals,intervals_size,0);
+    rb_ary_store(array,2,INT2FIX(index+1));
+
+  }else if (days_c > last_interval_c ) {
+
+    if (max_c < days_c) {
+      rb_ary_store(array,1,days);
+    }
+    rb_ary_store(array,2,INT2FIX(aggr_size -1));
   
+  }else {  
+     
+  }
+  //rb_ary_store(array,0,INT2FIX(first_interval));  
+  //rb_ary_push(array,StringValue("eiii"));
+  //rb_ary_store(array,2,INT2FIX(first_interval));
+  //if days < first_interval{
+  //  rb_ary_store(array,2,INT2FIX(first_interval));  
+  //}  
 
   return ST_CONTINUE;
 }  
 
-// 
-static VALUE teste(VALUE self, VALUE min, VALUE max, VALUE hash)
+// Function to find min, max and the period of the HashMapValue of trajectories in Visualize app
+// Return: [ min, max, period]
+static VALUE min_max_period(VALUE self, VALUE min, VALUE max, VALUE hash, VALUE intervals, VALUE aggr)
 {
 
-    int  min_c = FIX2INT(min);
-    int  max_c = FIX2INT(max);
-    int period = Qnil;
+    // Convert the Ruby Values to C values
+    //int  min_c = FIX2INT(min);
+    //int  max_c = FIX2INT(max);
+    int  period = Qnil;
+    //VALUE  period = T_NIL;
+    //int  intervals_size = RARRAY_LEN(intervals);
+
+    // Create Ruby array that will be the result
     VALUE array  = rb_ary_new();
 
-    rb_hash_foreach(hash,testando_hash,array);
+    // Push initial values of the result
+    rb_ary_push(array,min);  
+    rb_ary_push(array,max);  
+    rb_ary_push(array,period);  
+    rb_ary_push(array,intervals);  
+    rb_ary_push(array,aggr);  
+    
+    
+    // iterate on hash calling "encontra_min_max_period" for each
+    rb_hash_foreach(hash,encontra_min_max_period,array);
       
+    // Return results
     return array;
 }
 
 
-// Function to convert params into array 
-static VALUE create_array(VALUE self, VALUE min, VALUE max)
-{
 
-    int  min_c = FIX2INT(min);
-    int  max_c = FIX2INT(max);
-    int period = Qnil;
-    VALUE array  = rb_ary_new();
+static VALUE test(VALUE self, VALUE param){
 
-
-    VALUE str1 = INT2FIX(min_c);
-    VALUE str2 = INT2FIX(max_c);
-
-    rb_ary_push(array,str1);  
-    rb_ary_push(array,str2); 
-
-    return array;
-}
+  int a =  INT2FIX(param);
+  return FIX2INT(a);
+}  
 
 // Main function called when the gem is loaded 
 void Init_visualize_helper(void) {
@@ -60,9 +114,9 @@ void Init_visualize_helper(void) {
   // Register the method Hello World without parameters 
   rb_define_singleton_method(mVisualizeHelper, "hello_world", hello_world, 0);
 
-  // Register the method  teste
-  rb_define_singleton_method(mVisualizeHelper, "teste", teste, 3);
+  // Register the method min_max_period 
+  rb_define_singleton_method(mVisualizeHelper, "min_max_period", min_max_period, 5);
 
-  // Register the method create_array 
-  rb_define_singleton_method(mVisualizeHelper, "create_array", create_array, 2);
+  // Register the method  for development testing
+  rb_define_singleton_method(mVisualizeHelper, "test", test, 1 );
 }
--- lib/visualize_helper/version.rb
+++ lib/visualize_helper/version.rb
@@ -1,3 +1,3 @@
 module VisualizeHelper
-  VERSION = "0.0.10.25"
+  VERSION = "0.0.10.72"
 end
